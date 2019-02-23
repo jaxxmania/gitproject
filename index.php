fa32923420d8797ade27a06f4eaaa6e92c81d886
jaxxmania <jenishnxt@gmail.com>
2019-02-23 12:30:30 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JS Designs</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!--custom css-->
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+</head>
+ <body>
+   <header>
+   			<nav class="navbar navbar-change navbar-fixed-top" id="mynavbar">
+   				
+   					<div class="container-fluid">
+   							<div class="navbar-header">
+   									<a href="#" class="navbar-brand">MKS</a>  						
+   							</div>
+   						
+   					</div>
+   				
+   			</nav>
+   	
+   	
+   </header>
+   
+   
+   <!--Latest and minified  version of js-->
+   <script src="js/jquery-3.3.1.min.js"></script>
+   <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
